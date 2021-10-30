5d3e4fa341bb091d4b68fd15a7f10827d1d5c464
Gopichand <56589158+Gopichand246@users.noreply.github.com>
2021-10-30 09:37:23 +0530

    Update two_sum.py

--- twoSum/two_sum.py
+++ twoSum/two_sum.py
@@ -1,16 +1,21 @@
+
+
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+
 def solve(nums, target): 
      s=set()
      arr=[0,0]
      for elem in nums:
-         s.add(elem)
+         s.add(elem)  # all the elements in nums are added into a hash map
      for i in range(len(nums)):
-         if target-nums[i] in s:
-             arr[0]=i
+         if target - nums[i] in s:
+             arr[0] = i
      for j in range(len(nums)):
-         if nums[j]==target-nums[arr[0]] and j!=arr[0]:
+         if nums[j] == target-nums[arr[0]] and j!=arr[0]:
              arr[1]=j
-     return arr
+     return arr  #  an array is returned
 
-nums=[3,2,4]
-target=6
-print(solve(nums,target))
+nums = [3,2,4]  # sample input
+target = 6
+print(solve(nums, target))
