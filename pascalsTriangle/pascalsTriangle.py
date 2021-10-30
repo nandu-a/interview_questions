f8b2ef4c61f2377f68d0e8953f43769812604137
Lokesh-pavan-kumar <lokesh.7.8.kl@gmail.com>
2021-10-30 07:26:52 +0530

    Added the python solution for Pascals Triangle

---
+++ pascalsTriangle/pascalsTriangle.py
@@ -0,0 +1,18 @@
+
+
+def generate(numRows: int):
+    answer = [[1]]
+        
+    for i in range(1, numRows):
+        lastRow = [0] + answer[-1] + [0]
+        newRow = []
+        
+        for j in range(0, len(lastRow) - 1):
+            newRow.append(lastRow[j] + lastRow[j+1])
+            
+        answer.append(newRow)
+        
+    return answer
+
+print(generate(3))
+
